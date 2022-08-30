2fc4af91c86ccd46f43a4ea4923ebc03b006cb39
hugo <hugopereira.mg@gmail.com>
2022-08-30 20:16:23 -0300

    Alteracao do Hugo

--- projeto/Program.cs
+++ projeto/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+ 
+ /// Alteracao do Hugo
+
+ Console.WriteLine("Hello, World!");
+ Console.WriteLine("Hello, World!");
+ Console.WriteLine("Hello, World!");
+ Console.WriteLine("Hello, World!");
+ Console.WriteLine("Hello, World!");
+ Console.WriteLine("Hello, World!");
+ Console.WriteLine("Hello, World!");
+
+
+ 
