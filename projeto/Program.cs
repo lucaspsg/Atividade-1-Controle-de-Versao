36700e81f5a7b0f99a1f0bbcc6f3c914b59f08b0
lucasParraPJ <lucas.parra@polijunior.com.br>
2022-08-30 20:09:58 -0300

    Primeira alteracao

--- projeto/Program.cs
+++ projeto/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Alteração realizada por Lucas Parra Sgarbosa
+
+Console.WriteLine("Lucas123123123123");
