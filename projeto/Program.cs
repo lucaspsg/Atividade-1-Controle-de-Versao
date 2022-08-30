5d330d78f3139a27356377e5b7c471db1b19ffc5
lucasParraPJ <lucas.parra@polijunior.com.br>
2022-08-30 20:10:32 -0300

    Segunda alteracao

--- projeto/Program.cs
+++ projeto/Program.cs
@@ -4,3 +4,19 @@ Console.WriteLine("Hello, World!");
 // Alteração realizada por Lucas Parra Sgarbosa
 
 Console.WriteLine("Lucas123123123123");
+
+// Outra alteração realizada por Lucas Parra Sgarbosa
+
+string [,] matrix = new string[3,3];
+
+for(int i = 0; i < matrix.GetLength(0); i++){
+    for(int j = 0; j < matrix.GetLength(1); j++){
+        matrix[i, j] = "Lucas";
+    }
+}
+
+for(int i = 0; i < matrix.GetLength(0); i++){
+    for(int j = 0; j < matrix.GetLength(1); j++){
+        Console.WriteLine(matrix[i, j]);
+    }
+}
